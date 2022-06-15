ff50d47482ab0cdbc532ebaf725ec90b4f90f755
Menna Hassan <mhassan@envasetechnologies.com>
2022-06-15 21:36:16 +0200

    TEST: Test commit

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,7 +4,7 @@ import unittest
 class TestSampleClass(unittest.TestCase):
 
     def test_should_fail(self):
-        self.fail('You should remove this test')
+        self.fail('Test failing')
         
 
 if __name__=="__main__":
