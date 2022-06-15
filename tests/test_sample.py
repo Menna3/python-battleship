7a15ba66125320f22a6ab3d1c8d1faca960dffa6
Menna Hassan <mhassan@envasetechnologies.com>
2022-06-15 22:05:03 +0200

    TEST: Test commit 6

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,7 +4,7 @@ import unittest
 class TestSampleClass(unittest.TestCase):
 
     def test_should_fail(self):
-        self.fail('Test failing 5')
+        self.assertTrue(True)
         
 
 if __name__=="__main__":
